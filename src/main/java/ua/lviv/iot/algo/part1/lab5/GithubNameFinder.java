f2772a64fc92b5ec4979ee0e61be21142d5b01d5
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-04-30 22:24:18 +0300

    deleted main()

--- src/main/java/ua/lviv/iot/algo/part1/lab5/GithubNameFinder.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/GithubNameFinder.java
@@ -11,22 +11,13 @@ public class GithubNameFinder {
     public List<String> findGithubUsers(String text) {
         List<String> users = new ArrayList<>();
 
-        Pattern pattern = Pattern.compile("github\\.com/([^/]+)/");
+        Pattern pattern = Pattern.compile("(https://)?github\\.com/([^/]+)/");
         Matcher matcher = pattern.matcher(text);
 
         while (matcher.find()) {
-            users.add(matcher.group(1));
+            users.add(matcher.group(2));
         }
 
         return users;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        GithubNameFinder nameFinder = new GithubNameFinder();
-        System.out.println(nameFinder.findGithubUsers(input));
     }
 }
